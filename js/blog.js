cbb7af6bbb97d24e484fa48e197aade77c7c52b9
Ludvig Järvi <98786177+Jaevii@users.noreply.github.com>
2025-09-19 14:27:36 +0200

    profile updates

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,6 @@
 async function loadPosts() {
   // List of posts (add new JSON files here)
-  const postFiles = ["posts/post1.json", "posts/post2.json"];
+  const postFiles = ["posts/post1.json"];
 
   const container = document.getElementById("blog-container");
 
