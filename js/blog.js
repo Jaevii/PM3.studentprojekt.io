31579e9ee0d0375bea6b0cac348aa079e6e867e2
Ludvig Järvi <98786177+Jaevii@users.noreply.github.com>
2025-09-23 13:20:45 +0200

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -30,8 +30,8 @@ async function loadPosts() {
 
       // Use summary field for preview
       let summaryText = post.summary || '';
-      // Truncate summary for mobile devices
-      let isMobile = window.innerWidth <= 600;
+      // More robust mobile detection
+      let isMobile = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
       if (isMobile && summaryText.length > 120) {
         // Find a reasonable cut-off at a sentence or word boundary
         let cutIdx = summaryText.indexOf('.', 80);
